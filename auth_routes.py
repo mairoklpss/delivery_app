f379326687cb0b579ac764f1f3aa6092f04b8e38
mairoklpss <mairalopesma@gmail.com>
2026-08-27 13:41:59 -0300

    schema de usuário criado

--- auth_routes.py
+++ auth_routes.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from models import Usuario
 from dependencies import pegar_sessao
 from main import bcrypt_context
+from schemas import UsuarioSchema
 
 # roteador para autenticação
 # prefixo: caminho da rota, tags: subtitulo da documentação
@@ -18,19 +19,19 @@ async def home():
 
 @auth_router.post('/criar_conta')
 #função de criar conta, recebe email e senha como parâmetros e ambos sao STRING
-async def criar_conta(email: str, senha: str, nome: str, session=Depends(pegar_sessao)):
+async def criar_conta(usuario_schema: UsuarioSchema, session=Depends(pegar_sessao)):
 
     # verificar se o usuário com o mesmo email já existe no banco de dados
-    usuario = session.query(Usuario).filter(Usuario.email == email).first()
+    usuario = session.query(Usuario).filter(Usuario.email == usuario_schema.email).first()
     if usuario:
         # ja existe algum usuario
         raise HTTPException(status_code=400, detail="E-mail de Usuário já cadastrado") 
     else: 
-        senha_criptografada = bcrypt_context.hash(senha)
-        novo_usuario = Usuario(nome, email, senha_criptografada)
+        senha_criptografada = bcrypt_context.hash(usuario_schema.senha)
+        novo_usuario = Usuario(usuario_schema.nome, usuario_schema.email, senha_criptografada, usuario_schema.ativo, usuario_schema.admin)
         session.add(novo_usuario)
         # vai salvar o usuario
         session.commit()
-        return { "message": f"Usuário criado com sucesso: {email}"}
+        return { "message": f"Usuário criado com sucesso: {usuario_schema.email}"}
 
     
